be2f81fbbd95ecf7d91cb5bedcd34a9ee48eb229
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-10-13 21:57:14 -0300

    to do: remove already renamed subtitl

--- index.js
+++ index.js
@@ -5,23 +5,32 @@ const
      videoFiles = [],
      subtitleFiles = [];
 
-function RenomeiaRespectivo(selectedSubtitle){
-  /*  if(selectedSubtitle.search()){
-      fsystem.rename('./test/'+selectedSubtitle+'','./test/subtitlo.srt',
-      (err) => {
-        err ? console.log("er: " + err) : console.log("")
-    })
-
-  }  */   console.log(selectedSubtitle);
+function RelataErro(err){ 
+    err ? console.log("er: " + err) : console.log("")
 
 }
+
+function RenomeiaRespectivo(selectedSubtitle,selectedVideo){
+  let
+    numerosDoVid =
+     selectedVideo.replace(/\D+/g, "");
+
+   if(selectedSubtitle.search(numerosDoVid)){
+      /* fsystem.rename('./test/'+selectedSubtitle+'',
+        './test/'+'removeNum'+'',
+        RelataErro); */
+      console.log(selectedSubtitle);
+   }
+ }
 
 
 function AtualizaListagemVideoSubs(nomesArquivos){
   for (let fle in nomesArquivos){ 
     let 
       actualName = nomesArquivos[fle];
-        if(actualName.search(".srt")!=-1)//Se for legenda
+        if((actualName.search(".srt")!=-1)
+        ||
+           actualName.search(".ass")!=-1)//Se for legenda
           subtitleFiles.push(actualName);
           else
            videoFiles.push(actualName);    
@@ -40,7 +49,7 @@ function UsarTodosArquivos(err, namesOfFiles) {
     for(let subtitl in subtitleFiles){
       let
         selectedSubtitle = subtitleFiles[subtitl];        
-        RenomeiaRespectivo(selectedSubtitle)  
+        RenomeiaRespectivo(selectedSubtitle,selectedVideo);
     }
 
   }
